898cd77266580849502dbc1a5ece7a2f4ff38930
Elhambasir <elham1378basir@gmail.com>
2023-08-17 22:49:39 +0430

    Update expense.rb

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
 class Expense < ApplicationRecord
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
   has_many :group_expenses, foreign_key: :expense_id
-  has_and_belongs_to_many :groups
+  has_many :groups, through: :group_expenses, dependent: :destroy, source: :group
   validates_associated :group_expenses
   validates :name, presence: true, length: { minimum: 2 }
   validates :amount, presence: true, numericality: { greater_than: 0 }
