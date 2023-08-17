c0975ffab5e6c46bc4a11010eb2ec18f5373bb8d
Elhambasir <elham1378basir@gmail.com>
2023-08-17 22:37:49 +0430

    Add some data in the factoryBot  define

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { "basir" }
-    surname { "elham" }
-    email { "elham@gmail.com" }
-    password { "elham@123" }
+    name { 'basir' }
+    surname { 'elham' }
+    email { 'elham@gmail.com' }
+    password { 'elham@123' }
   end
 end
-
-
