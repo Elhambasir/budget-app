5a1b6c2713800f29c67358ff47ca8f91c84d5cb2
Elhambasir <elham1378basir@gmail.com>
2023-08-18 13:15:49 +0430

    Update expense_spec.rb
    Remove unused method

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -21,34 +21,18 @@ RSpec.describe Expense, type: :model do
     expect(@expense).to_not be_valid
   end
 
-  it 'Name must containe letter only' do
-    @expense.name = 'basir2'
-    expect(@expense).to_not be_valid
-  end
-
   it 'Amount must not be greater than 0' do
     @expense.amount = 0
     expect(@expense).to_not be_valid
   end
 
-  it 'most_recent_expense must return 3 most recent expenses' do
-    4.times do
-      FactoryBot.create(:expense, author: @author)
-    end
-    expect(@author.most_recent_expense.count).to eq(3)
+  it 'Amount must not be empty' do
+    @expense.amount = ''
+    expect(@expense).to_not be_valid
   end
 
-  it 'most_ancient_expense must return 3 most ancient expenses' do
-    4.times do
-      FactoryBot.create(:expense, author: @author)
-    end
-    expect(@author.most_ancient_expense.count).to eq(3)
+  it 'Amount must be a number' do
+    @expense.amount = 'l'
+    expect(@expense).to_not be_valid
   end
-
-  # it "total_expense must return sum of all user's expenses" do
-  #   4.times do
-  #     FactoryBot.create(:expense, author:@author, amount:10)
-  #   end
-  #   expect(@author.total_expense.to_digits).to eq(40)
-  # end
 end
