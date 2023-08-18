3cd70e4cc6006f7abde76a1602b497bb64a54269
Elhambasir <elham1378basir@gmail.com>
2023-08-18 14:27:11 +0430

    Create index.html.erb_spec.rb

---
+++ spec/views/expense/index.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "expense/index", type: :feature do
+  before(:each) do
+    @user = FactoryBot.create(:user)
+    @user.save
+    @group = FactoryBot.create(:group, author: @user)
+    @group.save
+    @expense = FactoryBot.create(:expense, author: @user)
+    @expense.save
+    sign_in @user
+  end
+
+  after do 
+    User.destroy_all
+  end
+
+  scenario "Login" do
+    visit new_user_session_path  
+    click_link @group.name
+    expect(page).to have_current_path(group_expenses_path(@group))
+    expect(page).to have_content("TRANSACTIONS")
+  end
+end
