d064c56b83cc4404f817a3c44d242069b561812b
Elhambasir <elham1378basir@gmail.com>
2023-08-18 13:17:08 +0430

    Update index.html.erb_spec.rb

--- spec/views/splash/index.html.erb_spec.rb
+++ spec/views/splash/index.html.erb_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
-
 RSpec.describe 'splash/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Must have a link to the login page' do
+    render
+    expect(rendered).to have_link('LOG IN')
+  end
+
+  it 'Must have a link to the registration page' do
+    render
+    expect(rendered).to have_link('SIGN UP')
+  end
 end
